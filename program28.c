e288b50af9fc77052b5543934dc39233feeca5a9
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-08-20 19:35:53 +0530

    Updating...
    Starting with Circular doubly linked list

---
+++ program28.c
@@ -0,0 +1,34 @@
+// CIRCULAR DOUBLY LINEAR LINKED LIST
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node * next;
+} NODE, *PNODE, **PPNODE;
+
+void InsertFirst(PPNODE Head, PPNODE Tail, int iNo){}
+
+void InsertLast(PPNODE Head, PPNODE Tail, int iNo){}
+
+void InsertAtLoc(PPNODE Head, PPNODE Tail, int iNo, int iLoc){}
+
+void DeleteFirst(PPNODE Head, PPNODE Tail){}
+
+void DeleteLast(PPNODE Head, PPNODE Tail){}
+
+void DeleteAtLoc(PPNODE Head, PPNODE Tail, int iLoc){}
+
+void Display(PNODE Head, PNODE Tail){}
+
+int Count(PNODE Head, PNODE Tail){return 0;}
+
+int main()
+{
+    PNODE First = NULL;
+    PNODE Last = NULL;
+    
+    return 0;
+}
